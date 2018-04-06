490e0acd40042f3c54066015d0c1c184cb9f1d91
MF <MF@ME.COM>
2018-04-06 16:38:40 +0300

    Add Department Web API Controller

---
+++ SchoolWebApp/Controllers/DepartmentApiController.cs
@@ -0,0 +1,133 @@
+﻿using SchoolWebApp.ApiModels;
+using SchoolWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SchoolWebApp.Controllers
+{
+    public class DepartmentApiController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/DepartmentApi
+        public IEnumerable<DepartmentApiModel> Get()
+        {
+            // You may use AutoMapper here
+            var list = new List<DepartmentApiModel>();
+            foreach (var item in db.Departments)
+            {
+                list.Add(new DepartmentApiModel { Id = item.Id, Name = item.Name });
+            }
+            return list;
+        }
+
+        // GET: api/DepartmentApi/5
+        [ResponseType(typeof(DepartmentApiModel))]
+        public IHttpActionResult Get(int id)
+        {
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            var model = new DepartmentApiModel { Id = department.Id, Name = department.Name };
+            return Ok(model);
+        }
+
+        // POST: api/DepartmentApi
+        [ResponseType(typeof(DepartmentApiModel))]
+        public IHttpActionResult PostDepartment(DepartmentApiModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Department department = new Department { Name = model.Name };
+            db.Departments.Add(department);
+            db.SaveChanges();
+
+            // Get the new Id from the database
+            model.Id = department.Id;
+
+            // Return status code 201 success with creation
+            return CreatedAtRoute("DefaultApi", new { id = department.Id }, model);
+        }
+
+        // This equivalent to UPDATE Operation
+        // PUT: api/DepartmentApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Put(int id, DepartmentApiModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != model.Id)
+            {
+                return BadRequest();
+            }
+
+            var department = new Department { Id = model.Id, Name = model.Name };
+            db.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/DepartmentApi/5
+        [ResponseType(typeof(DepartmentApiModel))]
+        public IHttpActionResult Delete(int id)
+        {
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            db.Departments.Remove(department);
+            db.SaveChanges();
+
+            var model = new DepartmentApiModel { Id = department.Id, Name = department.Name };
+            return Ok(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return db.Departments.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- SchoolWebApp/Global.asax.cs
+++ SchoolWebApp/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace SchoolWebApp
 {
@@ -13,6 +14,9 @@ namespace SchoolWebApp
     {
         protected void Application_Start()
         {
+            // Web API Registration
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
@@ -20,6 +24,12 @@ namespace SchoolWebApp
 
             // Register AutoMapper the app starts
             AutoMapperConfig.RegisterMappings();
+
+            // Added to format from entities to json
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+                .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
