47b28bb085f45a18a93b7dcb442f211b486e6093
MF <MF@ME.COM>
2018-04-07 09:32:33 +0300

    Add Examples of Charts using Chart.js

---
+++ SchoolWebApp/Controllers/ChartsController.cs
@@ -0,0 +1,44 @@
+﻿using SchoolWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolWebApp.Controllers
+{
+    public class ChartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Charts
+        public ActionResult Index()
+        {
+            // This chart displays on
+            // x-axes: Department names that contain one or more faculties
+            // y-axes: Number of faculties in each department
+
+            var departments = db.Departments.ToList();
+            var faculties = db.Faculties;
+            int count = 0;
+            var labels = new List<string>();
+            var data = new List<int>();
+            foreach (var item in departments)
+            {
+                // Find the number of faculties in the current department
+                count = faculties.Count(m => m.Department.Id == item.Id);
+                if(count != 0)
+                {
+                    labels.Add(item.Name);
+                    data.Add(count);
+                }
+            }
+
+            // Convert labels and data from lists to arrays and save them in ViewBag
+            ViewBag.Labels = labels.ToArray();
+            ViewBag.Data = data.ToArray();
+
+            return View();
+        }
+    }
+}
